4f5edf5892e249a9990572b5fc17c10f98fc3fd3
Miles Underwood <bigboyteenn@aol.com>
2017-06-08 11:12:13 -0400

    Step 7 done

--- New Unity Project 1/Assets/Scripts/PlayerController.cs
+++ New Unity Project 1/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour { 
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -27,6 +34,17 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 10)
+        {
+            winText.text = "You Win!";
         }
     }
 }
